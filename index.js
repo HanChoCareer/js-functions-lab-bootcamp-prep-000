9090a8afaa290d7ce8da67e02dd4758583709799
Hankyol Cho <hancho.career@gmail.com>
2018-06-19 19:25:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown (days, holiday){
-  return "It's ${days} days until ${holiday}!"
+  return `It's ${days} days until ${holiday}!`
 }
